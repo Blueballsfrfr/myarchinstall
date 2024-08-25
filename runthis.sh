7fecb84f420dc57376c329fdfc9a0b1eeb47db5d
bluebollsfrfr <134091727+Blueballsfrfr@users.noreply.github.com>
2024-08-25 20:45:53 +0100

    attempt fix :3 runthis.sh

--- runthis.sh
+++ runthis.sh
@@ -11,6 +11,7 @@ fi"
 clear
 echo "which disk would you like to install too?"
 lsblk | grep disk
+ehco 
 read -r DISK
 cfdisk /dev/"$DISK"
 $ERROR_CRASH
@@ -23,6 +24,7 @@ clear
 # shellcheck disable=SC2010
 echo "which device is the root partition $(ls /dev/ | grep "$DISK")"
 read -r ROOT_P
+wipefs -a /dev/"$ROOT_P"
 mkfs."$FILE_SYS" "$ROOT_P"
 mount /dev/"$ROOT_P" $B_DIR
 $ERROR_CRASH
@@ -31,6 +33,7 @@ clear
 # shellcheck disable=SC2010
 echo "which of these is your boot partition? $(ls /dev/ | grep "$DISK")"
 read -r BOOT_P
+wipefs -a /dev/"$BOOT_P"
 mkfs.fat -F 32 /dev/"$BOOT_P"
  mount /dev/"$BOOT_P" "$B_DIR"boot
 clear
@@ -38,6 +41,7 @@ clear
 echo "is there anyswap? if so choose your swap part! (leave blank if none) $(lsblk | grep disk)" 
 read -r SWAP_P
 if [ -n "$SWAP_P" ]; then
+    wipefs -a /dev/$SWAP_P
     swapon /dev/"$SWAP_P"
 fi
 clear
@@ -46,39 +50,41 @@ echo "which kernel would you like to use? (linux, linux-zen, linux-lts, linux-ha
 read -r KERNEL
 echo "what other packages would you like to use? eg: de/wm (base linux-firmware nano vim vi kitty rust git wget rust gcc and llvm all included)"
 read -r OTH_P
-pacstrap -K "$B_DIR" base linux-firmware nano vim vi kitty rust git wget rust gcc llvm sudo which grub efibootmgr reflector $KERNEL $OTH_P
+pacstrap -K "$B_DIR" base linux-firmware nano vim vi kitty rust git wget rust gcc llvm sudo which grub efibootmgr reflector "$KERNEL" "$OTH_P"
 genfstab -U "$B_DIR" >> /mnt/barchinstall/etc/fstab
-arch-chroot "$B_DIR" bash -c '
-    
-    echo '--save /etc/pacman.d/mirrorlist
+
+# shellcheck disable=SC1072
+# shellcheck disable=SC1073
+cat EOF << | arch-chroot $B_DIR
+    echo "--save /etc/pacman.d/mirrorlist
     --country United Kingdom
     --protocol https
-    --latest 5'>> /etc/xdg/reflector/reflector.conf
+    --latest 5">> /etc/xdg/reflector/reflector.conf
     systemctl enable reflector.service
     clear
 
-    echo Which Region are you in? $(ls /usr/share/zoneinfo)
+    echo "Which Region are you in? $(ls /usr/share/zoneinfo)"
     read REGION
     clear
-    echo Which City are you in/closest too?
+    echo "Which City are you in/closest too?"
     $(ls /usr/share/zoneinfo/"$REGION")
     read CITY
     clear
-    ln -sf /usr/share/zoneinfo/$REGION/$CITY
+    ln -sf /usr/share/zoneinfo/"$REGION"/"$CITY"
     hwclock --systohc
     clear
-    echo list all your locales now :)
+    echo 'list all your locales now :)'
     read LOCALES
-    echo $LOCALES >> /etc/locale.gen
+    echo "$LOCALES" >> /etc/locale.gen
     locale-gen
     clear
-    echo type your password now!
+    echo "type your password now!"
     passwd
-    echo do you want too add a user?
+    echo "do you want too add a user?"
     read USER_N
     if [ -n $USER_N ]; then
     useradd -mG wheel,video,input,audio $USER_N
-    echo "%wheel ALL=\(ALL:ALL)\ NOPASSWD: ALL" >> /etc/sudoers
+    echo "%wheel ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
     passwd $USER_N
     su $USER_N
     cd ~
@@ -98,7 +104,8 @@ arch-chroot "$B_DIR" bash -c '
     fi
     grub-install --target=x86_64-efi --efi-directory=/boot/efi
     grub-mkconfig -o /boot/grub/grub.cfg
-'
+EOF
+$ERROR_CRASH
 clear
 echo "do you wanna chroot in? y/n"
 read CHROOT_Y_N
